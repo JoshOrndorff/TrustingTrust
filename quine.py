4768fc194eefad6f9ff05f4a5ee1843df03e6599
Joshy Orndorff <admin@joshorndorff.com>
2019-01-04 16:10:37 -0500

    Quine working and simplified.

--- quine.py
+++ quine.py
@@ -1,17 +1,11 @@
-code = "\n\ndef unescape(char):\n    if char == \"\\\"\":\n        return \"\\\\\\\"\"\n    elif char == \"\\n\":\n        return \"\\\\n\"\n    else:\n        return char\n\n\nprint('code = \"', end=\"\")\nfor char in code:\n    print(unescape(char), end=\"\")\nprint('\"')\nprint(code)\n"
+code = """
+print("code = " + chr(34) + chr(34) + chr(34), end='')
+print(code, end='')
+print(chr(34) + chr(34) + chr(34))
+print(code)
+"""
 
-
-def unescape(char):
-    if char == "\"":
-        return "\\\""
-    elif char == "\n":
-        return "\\n"
-    else:
-        return char
-
-
-print('code = "', end="")
-for char in code:
-    print(unescape(char), end="")
-print('"')
+print("code = " + chr(34) + chr(34) + chr(34), end='')
+print(code, end='')
+print(chr(34) + chr(34) + chr(34))
 print(code)
